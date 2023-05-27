3d443de5250f10859d9f9f2b578d36648be34dd8
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2023-05-27 20:15:16 +0600

    set up routes

--- src/App.js
+++ src/App.js
@@ -6,19 +6,29 @@ import {ChakraProvider} from '@chakra-ui/react'
 import {theme} from "./chakraUI/Theme";
 
 import ProductList from "./products/product-list/ProductList";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
 
     return (
         <ChakraProvider theme={theme}>
 
-            <div className="App">
                 <BrowserRouter>
-                    <Routes>
-                        <Route path="/products" element={<ProductList/>}/>
-                    </Routes>
+                    <div className="grid grid-cols-6">
+                        <Navbar className="lg:col-span-1"/>
+                        <div className="lg:col-span-5 col-span-6">
+                            <Routes>
+                                <Route path="/" element={<ProductList/>}/>
+                                <Route path="/dashboard" element={<ProductList/>}/>
+                                <Route path="/products" element={<ProductList/>}/>
+                                <Route path="/orders" element={<ProductList/>}/>
+                                <Route path="/customers" element={<ProductList/>}/>
+                                <Route path= "*" element={<ProductList/>}/>
+                            </Routes>
+                        </div>
+                    </div>
+
                 </BrowserRouter>
-            </div>
         </ChakraProvider>
     );
 }
